2e9f80b424293c4da789c369e3970ff507a92173
Ganesh Epili <32766800+capgos17@users.noreply.github.com>
2019-12-05 00:54:21 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,16 +1,19 @@
 from flask import Flask, render_template, request
 from app import dateExtractor
+from werkzeug.utils import secure_filename
+import os
+
 app = Flask(__name__)
 
-# @app.route('/upload')
-# def upload_file():
-#    return render_template('upload.html')
 	
 @app.route('/extract_date', methods = ['POST'])
 def upload_file():
    if request.method == 'POST':
       file = request.files['base_64_image_content']
-      date=dateExtractor(file)
+      filename = secure_filename(file.filename) # save file 
+      filepath = os.path.join('./data', filename)
+      file.save(filepath)
+      date=dateExtractor(filepath)
       return date
 	
 if __name__ == '__main__':
